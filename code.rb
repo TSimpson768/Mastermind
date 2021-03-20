dee88f221ed73b34038658857a41811e68c46737
Thomas Simpson <thomas.simpson768@gmail.com>
2021-03-20 16:32:06 +0000

    Created an initialize function for a code

--- code.rb
+++ code.rb
@@ -1,5 +1,13 @@
 
 # Contains an individual code
 class Code
+
+  def initialize(peg1, peg2, peg3, peg4)
+    @pegs = []
+    @pegs[0] = peg1
+    @pegs[1] = peg2
+    @pegs[2] = peg3
+    @pegs[3] = peg4
+  end
   
 end
