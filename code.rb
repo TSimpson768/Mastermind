30209d1924c930ef5dfbc26d5d1ccb3dc74902b6
Thomas Simpson <thomas.simpson768@gmail.com>
2021-03-20 17:24:25 +0000

    Added a custom pegs getter to return a copy of pegs array

--- code.rb
+++ code.rb
@@ -9,25 +9,40 @@ class Code
     @pegs[2] = peg3
     @pegs[3] = peg4
   end
-  attr_reader :pegs
+
+  def pegs
+    pegs_copy = []
+    pegs_copy.replace(pegs)
+  end
 
   # Code -> [int, int]
   # TODO: Takes a code to compare against, and returns number of black (correct color + pos)
   # and white (correct color only) pegs to award.
   def check_code(code)
-    
+    num_black = black_pegs(code)
+    num_white = white_pegs(code)
+    num_white -= num_black
+    [num_black, num_white]
   end
 
   private
+
   # Code -> int
   # TODO; Returns number of exact matches between this code and given code
   def black_pegs(code)
-    
+    num_black = 0
+    code.pegs.each_with_index do |peg, index|
+      if peg == @pegs[index]
+        num_black += 1
+      end
+    end
+    num_black
   end
 
   # Code -> int
   # TODO; Return number of matched colored pegs between this code and given code
   def white_pegs(code)
+    
     
   end
 end
