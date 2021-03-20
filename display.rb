97727cbdf956c0d4a79e5d6b26d97a6674ba4ff8
Thomas Simpson <thomas.simpson768@gmail.com>
2021-03-20 16:22:34 +0000

    Added some display methods

--- display.rb
+++ display.rb
@@ -1,5 +1,19 @@
 # contains methods for outputing to the console
 
 module Display
+
+  # Code -> null
+  # TODO: Method for outputting a secret code
+  def render_code(code)
+    
+  end
+
+  # [int, int] -> null
+  # TODO: Method for rendering key pegs
+  def reder_pegs(pegs)
+    
+  end
+
+  # TODO: Further methods for text output and input
   
 end
