7280cb3ba504e3d6e24b9a9e854b1d23d8424d40
Thomas Simpson <thomas.simpson768@gmail.com>
2021-03-20 16:49:51 +0000

    Method definitions for code

--- code.rb
+++ code.rb
@@ -9,5 +9,25 @@ class Code
     @pegs[2] = peg3
     @pegs[3] = peg4
   end
-  
+  attr_reader :pegs
+
+  # Code -> [int, int]
+  # TODO: Takes a code to compare against, and returns number of black (correct color + pos)
+  # and white (correct color only) pegs to award.
+  def check_code(code)
+    
+  end
+
+  private
+  # Code -> int
+  # TODO; Returns number of exact matches between this code and given code
+  def black_pegs(code)
+    
+  end
+
+  # Code -> int
+  # TODO; Return number of matched colored pegs between this code and given code
+  def white_pegs(code)
+    
+  end
 end
