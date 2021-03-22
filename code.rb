2345809dc00a7ea6242d985a8a9e6da0f00f5a06
Thomas Simpson <thomas.simpson768@gmail.com>
2021-03-22 16:53:44 +0000

    Refactored code object to take an array for pegs

--- code.rb
+++ code.rb
@@ -2,12 +2,8 @@
 # Contains an individual code
 class Code
 
-  def initialize(peg1, peg2, peg3, peg4)
-    @pegs = []
-    @pegs[0] = peg1
-    @pegs[1] = peg2
-    @pegs[2] = peg3
-    @pegs[3] = peg4
+  def initialize(pegs)
+    @pegs = pegs
   end
 
   attr_reader :pegs
