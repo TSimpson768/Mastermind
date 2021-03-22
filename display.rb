d740a968f9d932142d44aafe5c6fd96b036648f4
Thomas Simpson <thomas.simpson768@gmail.com>
2021-03-22 16:52:57 +0000

    Wrote code to take code input

--- display.rb
+++ display.rb
@@ -2,6 +2,17 @@
 
 module Display
 
+  # TODO: Asks player for and returns a guess
+  def input_guess
+    puts 'Guess the code!'
+  begin
+    input = gets
+    parse(input)
+  rescue NoMethodError
+    input_error
+  end
+  
+  end
   # Code -> null
   # TODO: Method for outputting a secret code
   def render_code(code)
@@ -14,6 +25,23 @@ module Display
     
   end
 
-  # TODO: Further methods for text output and input
+  private
+
+  # TODO: Take a string and returns a code object.
+  # Regex should only match if 4 digits between 1-6 were input by user
+  def parse(input)
+    if /^[1-6]{4}$/.match(input)
+      code = input.split(//)
+      code.map!(&:to_i)
+      Code.new(code)
+    else
+      input_error
+    end
+  end
+
+  def input_error
+    puts 'Invalid input. Please enter a 4 number code, with each digit between 1-6.'
+    input_guess
+  end
   
 end
