bf14eee61e637f9a7e922606b023b65bdd553ff4
Thomas Simpson <thomas.simpson768@gmail.com>
2021-03-23 17:22:09 +0000

    Ensure previous guesses are saved

--- code_breaker.rb
+++ code_breaker.rb
@@ -1,17 +1,20 @@
+# frozen_string_literal: true
+
 # Contains methods for interacting with a human code breaker
 require_relative 'display'
 
+# CodeBreaker represents a human code breaker player, and all info about their game so far
 class CodeBreaker
-
   include Display
-  
   # TODO: Initialize any required class variables
   def initialize
     @previous_guesses = []
   end
 
-  # TODO: Return a valid guess from user input
+  # Return a valid guess from user input. Exists to be over-rided by computer breaker
   def make_guess
-    input_guess
+    guess = input_guess
+    @previous_guesses.push(guess)
+    guess
   end
 end
